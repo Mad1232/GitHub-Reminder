54fc91eb6db68e0900dbcfecd6d53075ce1a7582
Niraj Amin <39960123+NirajAmin@users.noreply.github.com>
2024-10-02 01:31:09 -0500

    Now retrieves data from postman

--- Experiments/namin_frontend_experiemnts/ExperimentalFrontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/AdminInventoryFragment.java
+++ Experiments/namin_frontend_experiemnts/ExperimentalFrontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/AdminInventoryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.FrameMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +13,22 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AdminInventoryFragment extends Fragment {
+    private static final String URL_JSON_ARRAY = "https://8671d7ff-5727-46d7-a203-c8589f137865.mock.pstmn.io/inventory";
+
     public AdminInventoryFragment() {
         // Required empty public constructor
     }
@@ -37,15 +52,43 @@ public class AdminInventoryFragment extends Fragment {
         TextView dataText = view.findViewById(R.id.data_text);
         //LinearLayout layout = getActivity().getWindow().getDecorView().getRootView().findViewById(R.id.admin_inventory_linearlayout);
 
-        for (String s : arr){
-//            TextView text = new TextView(getContext());
-//            text.setText(s);
-//            layout.addView(text);
-            dataText.setText(dataText.getText() + "\n" + s);
-        }
+        GetJSONData(dataText);
 
         return view;
     }
 
+    private void GetJSONData(TextView dataText) {
+        dataText.setText("goofy");
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(
+                Request.Method.GET,
+                URL_JSON_ARRAY,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        dataText.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        dataText.setText("Volley error");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+        VolleySingleton.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonArrReq);
+    }
 
 }
