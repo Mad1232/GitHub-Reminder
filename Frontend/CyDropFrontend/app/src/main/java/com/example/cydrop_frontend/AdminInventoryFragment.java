bb6eeeccb5d7f26057ad28d86d80ecfc03e86abc
Niraj Amin <39960123+NirajAmin@users.noreply.github.com>
2024-10-03 12:11:06 -0500

    Moved files to frontend folder

---
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/AdminInventoryFragment.java
@@ -0,0 +1,104 @@
+package com.example.cydrop_frontend;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.FrameMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AdminInventoryFragment extends Fragment {
+    private static final String URL_JSON_ARRAY = "https://8671d7ff-5727-46d7-a203-c8589f137865.mock.pstmn.io/inventory";
+
+    public AdminInventoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_admin_inventory, container, false);
+
+        // Test array
+        String[] arr = new String[]{"test1", "test2"};
+        TextView dataText = view.findViewById(R.id.data_text);
+
+        GetJSONData(dataText);
+
+        return view;
+    }
+
+    private void GetJSONData(TextView dataText) {
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(
+                Request.Method.GET,
+                URL_JSON_ARRAY,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONArray jsonArr = response;
+                            for (int i = 0; i < jsonArr.length(); i++){
+                                JSONObject jObj = jsonArr.getJSONObject(i);
+                                String newLine = "ID: " + jObj.getString("id") +
+                                        " | Product name: " + jObj.getString("product-name") +
+                                        " | Quantity: " + jObj.getInt("quantity") + "\n";
+                                dataText.setText(dataText.getText() + newLine);
+                            }
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        dataText.setText("Volley error");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+        VolleySingleton.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonArrReq);
+    }
+
+}
---
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/ClientNavbarMainActivity.java
@@ -0,0 +1,64 @@
+package com.example.cydrop_frontend;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.cydrop_frontend.databinding.ActivityClientNavbarMainBinding;
+
+
+public class ClientNavbarMainActivity extends AppCompatActivity {
+
+    private int userid = -1;
+
+    ActivityClientNavbarMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        EdgeToEdge.enable(this);
+        binding = ActivityClientNavbarMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        replaceFragment(new ClientRemindersFragment());
+
+        // extract data passed into this activity from another activity
+        Bundle extras = getIntent().getExtras();
+        userid = extras.getInt("USERID");  // this will come from LoginActivity
+
+
+        // Switch fragments when an icon is selected
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.reminders){
+                replaceFragment(new ClientRemindersFragment());
+            } else if (itemId == R.id.home) {
+                replaceFragment(new ClientHomeFragment());
+            } else { // itemId == questions
+                replaceFragment(new ClientQuestionsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
+
