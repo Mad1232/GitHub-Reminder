28cdd6d20ffa779026608fed3d75dbc8eaa93fef
Niraj Amin <39960123+NirajAmin@users.noreply.github.com>
2024-10-06 16:32:18 -0500

    Project now works with backend service, completing demo3miniassignment 3 for demo 2

--- Experiments/namin_frontend_experiemnts/ExperimentalFrontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/AdminInventoryFragment.java
+++ Experiments/namin_frontend_experiemnts/ExperimentalFrontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/AdminInventoryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
 import android.util.Log;
 import android.view.FrameMetrics;
 import android.view.LayoutInflater;
@@ -13,21 +14,24 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class AdminInventoryFragment extends Fragment {
-    private static final String URL_JSON_ARRAY = "https://8671d7ff-5727-46d7-a203-c8589f137865.mock.pstmn.io/inventory";
+    private static final String URL_JSON_ARRAY ="http://10.0.2.2:8080/petTest";
 
     public AdminInventoryFragment() {
         // Required empty public constructor
@@ -52,13 +56,16 @@ public class AdminInventoryFragment extends Fragment {
         TextView dataText = view.findViewById(R.id.data_text);
         //LinearLayout layout = getActivity().getWindow().getDecorView().getRootView().findViewById(R.id.admin_inventory_linearlayout);
 
-        GetJSONData(dataText);
-
+        try {
+            GetJSONData(dataText);
+//            GetJSONTest(dataText);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return view;
     }
 
-    private void GetJSONData(TextView dataText) {
-        dataText.setText("goofy");
+    private void GetJSONData(TextView dataText) throws IOException, NullPointerException{
         JsonArrayRequest jsonArrReq = new JsonArrayRequest(
                 Request.Method.GET,
                 URL_JSON_ARRAY,
@@ -72,7 +79,7 @@ public class AdminInventoryFragment extends Fragment {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        dataText.setText("Volley error");
+                        dataText.setText("Volley error: " + error.toString());
                     }
                 }) {
             @Override
@@ -91,4 +98,30 @@ public class AdminInventoryFragment extends Fragment {
         VolleySingleton.getInstance(getContext().getApplicationContext()).addToRequestQueue(jsonArrReq);
     }
 
+    private void GetJSONTest(TextView dataText) throws IOException, NullPointerException {
+        JsonRequest<String> StringReq = new JsonRequest<String>(
+                Request.Method.GET,
+                URL_JSON_ARRAY,
+                null, // Pass null as the request body since it's a GET request
+                response -> {
+                    dataText.setText("We got a response!");
+                    dataText.setText(response);
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                return null;
+            }
+        };
+
+
+
+        VolleySingleton.getInstance(getContext().getApplicationContext()).addToRequestQueue(StringReq);
+    }
 }
