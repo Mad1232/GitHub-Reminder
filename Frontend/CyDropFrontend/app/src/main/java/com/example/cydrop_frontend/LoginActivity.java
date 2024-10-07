8bac59728f7f01f6dfa3861fefeb5b3f03330480
Madison Vosburg <mvosburg@iastate.edu>
2024-10-06 23:31:38 -0500

    edited java files, trying to connect to signup page

--- Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/LoginActivity.java
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/LoginActivity.java
@@ -1,4 +1,84 @@
 package com.example.cydrop_frontend;
 
-public class LoginActivity {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class LoginActivity extends AppCompatActivity{
+    private EditText usernameEditText;  // define username edittext variable
+    private EditText passwordEditText;  // define password edittext variable
+    private Button loginButton;         // define login button variable
+    private Button signupButton;        // define signup button variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);            // link to Login activity XML
+
+        /* initialize UI elements */
+        usernameEditText = findViewById(R.id.login_username_edt);
+        passwordEditText = findViewById(R.id.login_password_edt);
+        loginButton = findViewById(R.id.login_login_btn);    // link to login button in the Login activity XML
+        signupButton = findViewById(R.id.login_signup_btn);  // link to signup button in the Login activity XML
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* grab strings from user inputs */
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+             //   Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+             //   intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
+             //   intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
+             //   startActivity(intent);  // go to MainActivity with the key-value data
+
+               // sendLogin(v, username, password);
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
+    }
+
+//    public void sendLogin(View v, String username, String password){
+//        String postURL = "http://coms-3090-038.class.las.iastate.edu:8080/users";
+//        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+//
+//        JsonObjectRequest jsonObjReq = new JsonArrayRequest(Request.Method.GET, postURL, null, new Response.Listener<JSONArray>() {
+//            @Override
+//            public void onResponse(JSONArray response) {
+//
+//            }
+//        }
+//    }
+
+
+    }
--- Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/SignupActivity.java
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/SignupActivity.java
@@ -1,4 +1,113 @@
 package com.example.cydrop_frontend;
 
-public class SignupActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SignupActivity extends AppCompatActivity{
+    private EditText usernameEditText;  // define username edittext variable
+    private EditText passwordEditText;  // define password edittext variable
+    private EditText typeEditText;   // define type edittext variable
+    private Button loginButton;         // define login button variable
+    private Button signupButton;        // define signup button variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        /* initialize UI elements */
+        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
+        passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
+        typeEditText = findViewById(R.id.signup_confirm_edt);    // link to confirm edtext in the Signup activity XML
+        loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
+        signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);  // go to LoginActivity
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String type = typeEditText.getText().toString();
+
+                register(username, password, type);
+            }
+        });
+    }
+
+
+public void register(String u, String p, String t){
+
+   // String URL = "http://10.0.2.2:8080/demo/api/login/addUser";
+    String URL = "https://6fa018b4-c1a0-496a-bfae-f630fd6c2a65.mock.pstmn.io/test";
+
+    JSONObject postBody = new JSONObject();
+    try{
+        postBody.put("username", u);
+        postBody.put("password", p);
+        postBody.put("type", t);
+    }catch(Exception e){
+        e.printStackTrace();
+    }
+
+    JsonObjectRequest request = new JsonObjectRequest(
+            Request.Method.POST,
+            URL,
+            postBody,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Toast.makeText(SignupActivity.this, "Signup Successful!", Toast.LENGTH_LONG).show();
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(SignupActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+    ) {
+        @Override
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/json");
+            return headers;
+        }
+    };
+
+    // Add the request to the Volley request queue
+    VolleySingleton.getInstance(this).addToRequestQueue(request);
 }
+
+}
+
