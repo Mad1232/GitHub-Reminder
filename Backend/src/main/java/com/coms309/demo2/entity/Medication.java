5f91ed2f4600b77a3f489742f67cb8c259fbfc3e
Prakarsha <musicstyle1232@gmail.com>
2024-10-15 15:07:21 -0500

    Added OneToOne for Pets & Medications

--- Backend/src/main/java/com/coms309/demo2/entity/Medication.java
+++ Backend/src/main/java/com/coms309/demo2/entity/Medication.java
@@ -3,12 +3,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 public class Medication {
     @Id
     @Getter
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) //had to do this to make it work
+    Long medication_id; //changed from id to avoid confusion
 
     @Getter
     @Setter
@@ -17,4 +19,17 @@ public class Medication {
     @Getter
     @Setter
     Integer stock;
+
+    // One-to-many relationship with Pet
+    @OneToOne(mappedBy = "medication", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Pet pet;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
 }
