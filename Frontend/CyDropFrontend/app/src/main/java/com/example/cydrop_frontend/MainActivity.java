3ed594400f4a082d15ef66f2697775560540b7ae
Niraj Amin <39960123+NirajAmin@users.noreply.github.com>
2024-10-17 12:07:01 -0500

    login saved

--- Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/MainActivity.java
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Try to see if login is saved
         try {
-            VolleySingleton.userId = getPreferences(Context.MODE_PRIVATE).getString("userId", "-1");
-            VolleySingleton.userType = getPreferences(Context.MODE_PRIVATE).getString("userType", "none");
+            SharedPreferences sharedPref =  PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            VolleySingleton.userId = sharedPref.getString("userId", "-1");
+            VolleySingleton.userType = sharedPref.getString("userType", "none");
             if (!Objects.equals(VolleySingleton.userId, "-1") && !VolleySingleton.userType.equals("none")){
                 Intent intent = new Intent(MainActivity.this, ClientNavbarMainActivity.class);
                 switch (VolleySingleton.userType){
