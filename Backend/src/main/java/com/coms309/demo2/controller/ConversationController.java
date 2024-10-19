934ea99fd6b79079b0be7a0796cb660b6472306c
EmmyTheOptimist <macarydis@gmail.com>
2024-10-19 17:45:21 -0500

    implement get all messages from conversation

--- Backend/src/main/java/com/coms309/demo2/controller/ConversationController.java
+++ Backend/src/main/java/com/coms309/demo2/controller/ConversationController.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coms309.demo2.entity.Conversation;
+import com.coms309.demo2.entity.ConversationKey;
 import com.coms309.demo2.entity.User;
+import com.coms309.demo2.entity.Vet;
 import com.coms309.demo2.repository.ConversationRepository;
+import com.coms309.demo2.repository.MessageRepository;
 import com.coms309.demo2.repository.UserRepository;
 import com.coms309.demo2.repository.VetsRepo;
 
@@ -28,9 +31,9 @@ public class ConversationController {
     @Autowired
     VetsRepo vetRepository;
 
-    //get list of prior conversations
+    //get list of prior conversations for a user
     @GetMapping("/users/{id}/conversations")
-    public List<Conversation> getAllConversations(@PathVariable Long id) {
+    public List<Conversation> getAllConversationsUser(@PathVariable Long id) {
         Optional<User> userOptional = userRepository.findById(id);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
@@ -41,5 +44,66 @@ public class ConversationController {
             throw new RuntimeException("User does not exist :(");
         }
     }
+
+    //get list of prior conversations for a vet
+    @GetMapping("/vet/{id}/conversations")
+    public List<Conversation> getAllConversationsVet(@PathVariable Integer id) {
+        Optional<Vet> vetOptional = vetRepository.findById(id);
+        if (vetOptional.isPresent()) {
+            Vet vet = vetOptional.get();
+            List<Conversation> conversations = conversationRepository.findByVet(vet);
+            return conversations;
+        }
+        else {
+            throw new RuntimeException("Vet does not exist :(");
+        }
+    }
     
+    //get all messages for a conversation with a vet
+    @GetMapping("/users/{idUser}/conversations/{idVet}")
+    public Conversation getAllMessagesUser(@PathVariable Long idUser, @PathVariable Integer idVet) {
+
+        Optional<User> userOptional = userRepository.findById(idUser);
+        Optional<Vet> vetOptional = vetRepository.findById(idVet);
+        if (userOptional.isPresent() && vetOptional.isPresent()) {
+            User user = userOptional.get();
+            Vet vet = vetOptional.get();
+
+            Optional<Conversation> conversationOptional = conversationRepository.findById(new ConversationKey(user, vet));
+            if (conversationOptional.isPresent()) {
+                Conversation conversation = conversationOptional.get();
+                return conversation;
+            }
+            else {
+                throw new RuntimeException("Conversation does not exist");
+            }
+        }
+        else {
+            throw new RuntimeException("User and/or Vet does not exist :(");
+        }
+    }
+
+
+    //get all messages for a conversation with a user
+    @GetMapping("/vet/{idVet}/conversations/{idUser}")
+    public Conversation getAllMessagesVet(@PathVariable Integer idVet, @PathVariable Long idUser) {
+        Optional<Vet> vetOptional = vetRepository.findById(idVet);
+        Optional<User> userOptional = userRepository.findById(idUser);
+        if (vetOptional.isPresent() && userOptional.isPresent()) {
+            Vet vet = vetOptional.get();
+            User user = userOptional.get();
+            Optional<Conversation> conversationOptional = conversationRepository.findById(new ConversationKey(user, vet));
+            if (conversationOptional.isPresent()) {
+                Conversation conversation = conversationOptional.get();
+                return conversation;
+            }
+            else {
+                throw new RuntimeException("Conversation does not exist");
+            }
+        }
+        else {
+            throw new RuntimeException("Vet and/or User does not exist :(");
+        }
+    }
+
 }
--- Backend/src/main/java/com/coms309/demo2/entity/ConversationKey.java
+++ Backend/src/main/java/com/coms309/demo2/entity/ConversationKey.java
@@ -1,7 +1,13 @@
 package com.coms309.demo2.entity;
 import java.io.Serializable;
 
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
 public class ConversationKey implements Serializable {
+    @NonNull
     User user;
+    @NonNull
     Vet vet;
 }
