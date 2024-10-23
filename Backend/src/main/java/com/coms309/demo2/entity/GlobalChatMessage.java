32ebeb551e48b3153c07a027aaa21857ce26f245
Prakarsha <musicstyle1232@gmail.com>
2024-10-23 11:04:09 -0500

    Added global chat websockets

---
+++ Backend/src/main/java/com/coms309/demo2/entity/GlobalChatMessage.java
@@ -0,0 +1,44 @@
+package com.coms309.demo2.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "globalchat_messages")
+public class GlobalChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sender;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    private LocalDateTime timestamp;
+
+    public GlobalChatMessage() {}
+
+    public GlobalChatMessage(String sender, String message, LocalDateTime timestamp) {
+        this.sender = sender;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+    public void setMessage(String message) {
+        this.message = message;}
+
+}
