8bec69b43c48038cdf782f8f3b63b47054322411
EmmyTheOptimist <macarydis@gmail.com>
2024-10-29 18:31:49 -0500

    update repository

--- Backend/src/main/java/com/coms309/demo2/repository/MessageRepository.java
+++ Backend/src/main/java/com/coms309/demo2/repository/MessageRepository.java
@@ -2,7 +2,10 @@ package com.coms309.demo2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.coms309.demo2.entity.Conversation;
 import com.coms309.demo2.entity.Message;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByConversation(Conversation conversation);
 }
