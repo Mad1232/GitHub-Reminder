7a40fe9684b6a7088aa067e9f6849f4eb5502a9f
EmmyTheOptimist <macarydis@gmail.com>
2024-10-30 14:20:53 -0500

    fix path

--- Backend/src/main/java/com/coms309/demo2/repository/RepositoryInjector.java
+++ Backend/src/main/java/com/coms309/demo2/repository/RepositoryInjector.java
@@ -1,6 +1,6 @@
 package com.coms309.demo2.repository;
 
-import com.coms309.demo2.config.GlobalChatServer;
+import com.coms309.demo2.websocket.GlobalChatServer;
 import com.coms309.demo2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
