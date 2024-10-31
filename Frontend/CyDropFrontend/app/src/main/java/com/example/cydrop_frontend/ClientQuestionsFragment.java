951940b5dddb13d3d2e4a542831588b6f9e7ee95
Madison Vosburg <mvosburg@iastate.edu>
2024-10-30 22:38:10 -0500

    added pathway for vet chat page

--- Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/ClientQuestionsFragment.java
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/ClientQuestionsFragment.java
@@ -16,7 +16,7 @@ import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ClientQuestionsFragment extends Fragment implements WebSocketListener{
+public class ClientQuestionsFragment extends Fragment{
     TextView messageBody;
     EditText messageInputText;
     private Button client_questions_direct_chat_button;
@@ -36,46 +36,14 @@ public class ClientQuestionsFragment extends Fragment implements WebSocketListen
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_client_questions, container, false);
 
-        // Try to connect to websocket
-        WebSocketManager.getInstance().connectWebSocket("ws://coms-3090-038.class.las.iastate.edu:8080/chat/" + VolleySingleton.email);
-        WebSocketManager.getInstance().setWebSocketListener(ClientQuestionsFragment.this);
-
-        messageBody = view.findViewById(R.id.websockt_testview);
-        messageBody.setText("ws://coms-3090-038.class.las.iastate.edu:8080/chat/" + VolleySingleton.email);
-
-        messageInputText = view.findViewById(R.id.client_questions_messagebox);
-
-        view.findViewById(R.id.client_questions_send_button).setOnClickListener(view2 -> {
-            WebSocketManager.getInstance().sendMessage(messageInputText.getText().toString());
-        });
 
         // Button to direct chat
         view.findViewById(R.id.client_questions_direct_chat_button).setOnClickListener(view2 -> {
-            Intent intent = new Intent(getActivity(), CustomerChatActivity.class);
+            Intent intent = new Intent(getActivity(), VetChatActivity.class);
             startActivity(intent);
         });
 
-        return  view;
-
+        return view;
     }
 
-    @Override
-    public void onWebSocketOpen(ServerHandshake handshakedata) {
-
-    }
-
-    @Override
-    public void onWebSocketMessage(String message) {
-        messageBody.setText(message);
-    }
-
-    @Override
-    public void onWebSocketClose(int code, String reason, boolean remote) {
-
-    }
-
-    @Override
-    public void onWebSocketError(Exception ex) {
-
-    }
 }
--- Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/VetQuestionsFragment.java
+++ Frontend/CyDropFrontend/app/src/main/java/com/example/cydrop_frontend/VetQuestionsFragment.java
@@ -1,5 +1,6 @@
 package com.example.cydrop_frontend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,72 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VetQuestionsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class VetQuestionsFragment extends Fragment {
+import org.java_websocket.handshake.ServerHandshake;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class VetQuestionsFragment extends Fragment implements WebSocketListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView messageBody;
+    EditText messageInputText;
+    private Button client_questions_direct_chat_button;
 
     public VetQuestionsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VetQuestionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VetQuestionsFragment newInstance(String param1, String param2) {
-        VetQuestionsFragment fragment = new VetQuestionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vet_questions, container, false);
+        View view = inflater.inflate(R.layout.fragment_vet_questions, container, false);
+
+        // Try to connect to websocket
+        WebSocketManager.getInstance().connectWebSocket("ws://coms-3090-038.class.las.iastate.edu:8080/chat/" + VolleySingleton.email);
+        WebSocketManager.getInstance().setWebSocketListener(VetQuestionsFragment.this);
+
+        messageBody = view.findViewById(R.id.websockt_testview);
+        messageBody.setText("ws://coms-3090-038.class.las.iastate.edu:8080/chat/" + VolleySingleton.email);
+
+        messageInputText = view.findViewById(R.id.client_questions_messagebox);
+
+        view.findViewById(R.id.client_questions_send_button).setOnClickListener(view2 -> {
+            WebSocketManager.getInstance().sendMessage(messageInputText.getText().toString());
+        });
+
+        // Button to direct chat
+        view.findViewById(R.id.client_questions_direct_chat_button).setOnClickListener(view2 -> {
+            Intent intent = new Intent(getActivity(), CustomerChatActivity.class);
+            startActivity(intent);
+        });
+
+        return  view;
+
+    }
+
+    @Override
+    public void onWebSocketOpen(ServerHandshake handshakedata) {
+
+    }
+
+    @Override
+    public void onWebSocketMessage(String message) {
+        messageBody.setText(message);
+    }
+
+    @Override
+    public void onWebSocketClose(int code, String reason, boolean remote) {
+
+    }
+
+    @Override
+    public void onWebSocketError(Exception ex) {
+
     }
 }
