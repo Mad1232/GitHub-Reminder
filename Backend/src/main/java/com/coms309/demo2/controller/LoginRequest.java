e613d1fa5f8f94bd500f126485f29fb2c274276b
EmmyTheOptimist <macarydis@gmail.com>
2024-11-15 23:48:00 -0600

    add javadoc comments to Login Request Controller

--- Backend/src/main/java/com/coms309/demo2/controller/LoginRequest.java
+++ Backend/src/main/java/com/coms309/demo2/controller/LoginRequest.java
@@ -8,29 +8,52 @@ public class LoginRequest {
     private String email;
     private String password;
 
-    // Default constructor
+    /**
+     * Default constructor
+     */
     public LoginRequest() {
     }
 
     // Constructor with parameters
+    /**
+     * Constructs a login request
+     * @param email attempted email
+     * @param password attempted password
+     */
     public LoginRequest(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
     // Getters and setters
+    /**
+     * Get email
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Set email
+     * @param email email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Get password
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Set password
+     * @param password password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
